b55346b0395186eb8639809fe4e5004c47166fa4
Roman <rart05@yahoo.com>
2025-02-06 17:37:42 +0100

    feat: added map component and service

--- src/app/components/od/create-event-component/create-event-component.component.ts
+++ src/app/components/od/create-event-component/create-event-component.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {MapComponent} from "../../utils/map/map.component";
 
 @Component({
-  selector: 'app-create-event-component',
-  imports: [],
-  templateUrl: './create-event-component.component.html',
+	selector: 'app-create-event-component',
+	imports: [
+		MapComponent
+	],
+	templateUrl: './create-event-component.component.html',
 })
-export class CreateEventComponent {}
+export class CreateEventComponent {
+}
