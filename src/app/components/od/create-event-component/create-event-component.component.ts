21cce2a9b45a7568788264c53ca73dfc84a117f9
Roman <rart05@yahoo.com>
2025-04-12 17:59:30 +0200

    feat : added location selection via map Leafleat for new event

--- src/app/components/od/create-event-component/create-event-component.component.ts
+++ src/app/components/od/create-event-component/create-event-component.component.ts
@@ -1,12 +1,46 @@
-import {Component} from '@angular/core';
-import {MapComponent} from "../../utils/map/map.component";
+import {Component, ViewChild} from '@angular/core';
+import {MapComponent} from '../../utils/map/map.component';
+import {FormsModule} from "@angular/forms";
+import {NgIf} from "@angular/common";
 
 @Component({
-	selector: 'app-create-event-component',
-	imports: [
-		MapComponent
-	],
+	selector: 'app-create-event',
 	templateUrl: './create-event-component.component.html',
+	imports: [
+		MapComponent,
+		FormsModule,
+		NgIf
+	]
 })
 export class CreateEventComponent {
+	showMap: boolean = false;
+	selectedLocation: {
+		lat: number;
+		lng: number;
+		address: string
+	} | null = null;
+
+	@ViewChild(MapComponent)
+	mapComponent!: MapComponent;
+
+	openMap(): void {
+		this.showMap = true;
+	}
+
+	closeMap(): void {
+		this.showMap = false;
+	}
+
+	onSelectLocation(): void {
+		if (this.mapComponent && this.mapComponent.selectedLocation) {
+			this.selectedLocation = this.mapComponent.selectedLocation;
+			this.showMap = false;
+		} else {
+			alert('Select a location on the map first.');
+		}
+	}
+
+	onSubmit(): void {
+
+	}
 }
