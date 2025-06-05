7b6e5f16dba6e0b8632b167a30741f29da5e3729
Roman <rart05@yahoo.com>
2025-06-05 15:30:35 +0200

    feat: added budget gen button

--- src/app/types/models/event.model.ts
+++ src/app/types/models/event.model.ts
@@ -1,22 +1,22 @@
-import {EventType} from "../eventType";
-import {AgendaItem} from "./agendaItem.model";
-import {BudgetItem} from "./budgetItem.model";
+import { EventType } from '../eventType';
+import { AgendaItem } from './agendaItem.model';
+import { BudgetItem } from './budgetItem.model';
 
 export interface Event {
-	id: string;
-	name: string;
-	description: string;
-	startDate: string;
-	endDate: string;
-	maxNumGuests: number;
-	isPrivate: boolean;
-	eventType: EventType;
-	location: {
-		lat: number;
-		lng: number;
-		address: string;
-	} | null;
-	agenda: AgendaItem[] | null;
-	budget: BudgetItem[] | null;
-	imageUrls: string[];
+  id: string;
+  name: string;
+  description: string;
+  startDate: string;
+  endDate: string;
+  maxNumGuests: number;
+  isPrivate: boolean;
+  eventType: EventType;
+  location: {
+    lat: number;
+    lng: number;
+    address: string;
+  } | null;
+  agenda: AgendaItem[] | null;
+  budget: BudgetItem[] | null;
+  imageUrls: string[];
 }
