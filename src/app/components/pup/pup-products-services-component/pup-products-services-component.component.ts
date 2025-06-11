c7f00013928c0490f5c736b14e41781df1a48785
Roman <rart05@yahoo.com>
2025-06-11 13:48:04 +0200

    feat: added products list on pup service page

--- src/app/components/pup/pup-products-services-component/pup-products-services-component.component.ts
+++ src/app/components/pup/pup-products-services-component/pup-products-services-component.component.ts
@@ -1,10 +1,47 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ServiceProductService } from '../../../services/service-products/service-products.service';
+import { ServiceProduct } from '../../../types/models/service-product.model';
 
 @Component({
   selector: 'app-pup-products-services-component',
+  standalone: true,
   imports: [RouterModule, CommonModule],
   templateUrl: './pup-products-services-component.component.html',
 })
-export class PupProductsServicesComponent {}
+export class PupProductsServicesComponent implements OnInit {
+  services: ServiceProduct[] = [];
+  isLoading = false;
+
+  constructor(private serviceProductService: ServiceProductService) {}
+
+  ngOnInit(): void {
+    this.loadUserServices();
+  }
+
+  private loadUserServices(): void {
+    this.isLoading = true;
+    this.serviceProductService.getMyServices().subscribe({
+      next: (services) => {
+        this.services = services;
+        this.isLoading = false;
+      },
+      error: (error) => {
+        console.error('Error loading services:', error);
+        this.isLoading = false;
+      },
+    });
+  }
+
+  formatPrice(price: number): string {
+    return new Intl.NumberFormat('sr-RS', {
+      style: 'currency',
+      currency: 'RSD',
+    }).format(price);
+  }
+
+  formatDate(date: Date): string {
+    return new Date(date).toLocaleDateString();
+  }
+}
